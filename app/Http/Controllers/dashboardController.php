ed98e3c881c78406ec54be3da9254f97ac42b785
nbeelnabzz <nabeelzamor@gmail.com>
2021-06-18 12:44:19 +0530

    changed

---
+++ app/Http/Controllers/dashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\news;
+use App\Models\commends;
+
+class dashboardController extends Controller
+{
+    public function dash(){
+        $news_count = news::count();
+        $comments_count = comments::count();
+        // $total_news_registration = news::count();
+        
+             return view('admin.dashboard',['news_count'=>$news_count,'comments_count'=>$comments_count]);
+    }
+}
